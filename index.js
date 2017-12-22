086670459790bb90d1e2a48330e1645eff5586aa
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-12-22 23:53:13 +0100

    Add topics with basic graphql query support

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 const Botkit = require("botkit");
 
-const controller = Botkit.slackbot({
-  //   debug: true
-});
+const bot = require("./lib/bot");
 
-// give the bot something to listen for.
-controller.hears(
-  ".*",
-  ["direct_message", "direct_mention", "mention"],
-  (bot, message) => {
-    console.log(message);
-    bot.reply(message, `Hello yourself. ${message.text}`);
-  }
-);
+const GRAPHQL_URL =
+  process.env.GRAPHQL_URL || "https://www.universe.com/graphql";
 
-controller
-  .spawn({
-    token: process.env.SLACK_TOKEN
-  })
-  .startRTM();
+const start = async url => {
+  const controller = Botkit.slackbot({
+    // debug: true
+  });
+
+  await bot.initializeWithURL(controller, url);
+
+  // give the bot something to listen for.
+  // controller.hears(
+  //   ".*",
+  //   ["direct_message", "direct_mention", "mention"],
+  //   (bot, message) => {
+  //     console.log(message);
+  //     bot.reply(message, `Hello yourself. ${message.text}`);
+  //   }
+  // );
+
+  // controller.hears(["hello", "hi"], ["direct_message"], (bot, message) => {
+  //   bot.startConversation(message, function(err, convo) {
+  //     convo.addQuestion("How are you?", function(response, convo) {
+  //       convo.say("Cool, you said: " + response.text);
+  //       convo.addQuestion("Anything else?2", (response, convo) => {
+  //         convo.say("bye2");
+  //         convo.next();
+  //       });
+  //       convo.next();
+  //     });
+  //     convo.addQuestion("Anything else?", (response, convo) => {
+  //       convo.say("bye");
+  //       convo.next();
+  //     });
+  //   });
+  // });
+
+  controller
+    .spawn({
+      token: process.env.SLACK_TOKEN
+    })
+    .startRTM();
+};
+
+start(GRAPHQL_URL);
